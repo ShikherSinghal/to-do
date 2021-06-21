1488f55f5dc39e19c1264e7007dd732f50c98be8
shikher.s@greyorange.com <GREYORANGE\shikher.s@ggn001253.greyorange.com>
2021-06-21 23:53:20 +0530

    Writing basic test

--- apps/todo/tests.py
+++ apps/todo/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import ToDoItems
+
+class ToDoItemCase(TestCase):
+    def setUp(self):
+        ToDoItems.objects.create(title="test 1")
+        ToDoItems.objects.create(name="test 2")
+
+    def test_to_do_created(self):
+        test_1 = ToDoItems.objects.get(title="test 1")
+        test_2 = AnToDoItemsimal.objects.get(name="test 2")
+        self.assertEqual(len(test_1), 5)
+        self.assertEqual(len(test_1), 5)
