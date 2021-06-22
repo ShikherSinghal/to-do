5a06602fe8d12a4f791b93438e490bcb6f525dc6
ShikherSinghal <31547710+ShikherSinghal@users.noreply.github.com>
2021-06-23 00:32:03 +0530

    Fixing test case

--- apps/todo/tests.py
+++ apps/todo/tests.py
@@ -6,10 +6,10 @@ from .models import ToDoItems
 class ToDoItemCase(TestCase):
     def setUp(self):
         ToDoItems.objects.create(title="test 1")
-        ToDoItems.objects.create(name="test 2")
+        ToDoItems.objects.create(title="test 2")
 
     def test_to_do_created(self):
         test_1 = ToDoItems.objects.get(title="test 1")
-        test_2 = AnToDoItemsimal.objects.get(name="test 2")
+        test_2 = AnToDoItemsimal.objects.get(title="test 2")
         self.assertEqual(len(test_1), 5)
         self.assertEqual(len(test_1), 5)
