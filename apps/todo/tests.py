d4c6acf4242fddf6e2a90c6c752046459196525e
shikher.s@greyorange.com <GREYORANGE\shikher.s@ggn001253.greyorange.com>
2021-06-23 00:38:58 +0530

    Fixing test

--- apps/todo/tests.py
+++ apps/todo/tests.py
@@ -5,11 +5,11 @@ from .models import ToDoItems
 
 class ToDoItemCase(TestCase):
     def setUp(self):
-        ToDoItems.objects.create(title="test 1")
-        ToDoItems.objects.create(title="test 2")
+        ToDoItems.objects.create(title="test1")
+        ToDoItems.objects.create(title="test2")
 
     def test_to_do_created(self):
-        test_1 = ToDoItems.objects.get(title="test 1")
-        test_2 = AnToDoItemsimal.objects.get(title="test 2")
-        self.assertEqual(len(test_1), 5)
-        self.assertEqual(len(test_1), 5)
+        test_1 = ToDoItems.objects.get(title="test1")
+        test_2 = ToDoItems.objects.get(title="test2")
+        self.assertEqual(len(test_1.title), 5)
+        self.assertEqual(len(test_2.title), 5)
